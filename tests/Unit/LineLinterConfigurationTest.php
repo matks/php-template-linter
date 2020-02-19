8d9d8022ec62ccd95bc94e3b4cd26a3785c891b0
matks <mathieu.ferment@prestashop.com>
2020-02-19 16:39:08 +0100

    Add unit tests for configuration

---
+++ tests/Unit/LineLinterConfigurationTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace MatksTests\Unit;
+
+use Matks\PHPTemplateLinter\DefaultConfiguration;
+use Matks\PHPTemplateLinter\LineLinter;
+use Matks\PHPTemplateLinter\LineLinterConfiguration;
+use Matks\PHPTemplateLinter\LineLinterConfigurationItem;
+use Matks\PHPTemplateLinter\LineLinterInput;
+use Matks\PHPTemplateLinter\LineLinterResult;
+use PHPUnit\Framework\TestCase;
+
+class LineLinterConfigurationTest extends TestCase
+{
+    public function testConfigurationOne()
+    {
+        $configuration = $this->buildConfiguration();
+
+        $linter = new LineLinter($configuration, false);
+
+        $line = 'b';
+        $input = new LineLinterInput(1, $line, 2, 0);
+        $result = $linter->fixLineIndentation($input);
+
+        $this->assertInstanceOf(LineLinterResult::class, $result);
+        $this->assertEquals(LineLinter::OPERATION_FIXED, $result->operationPerformed);
+        $this->assertEquals('  b', $result->lintedLine);
+    }
+
+    public function testConfigurationTwo()
+    {
+        $configuration = $this->buildConfiguration();
+
+        $linter = new LineLinter($configuration, false);
+
+        $line = '  c';
+        $input = new LineLinterInput(1, $line, 2, 0);
+        $result = $linter->fixLineIndentation($input);
+
+        $this->assertInstanceOf(LineLinterResult::class, $result);
+        $this->assertEquals(LineLinter::OPERATION_FIXED, $result->operationPerformed);
+        $this->assertEquals('c', $result->lintedLine);
+    }
+
+    public function testConfigurationThree()
+    {
+        $configuration = $this->buildConfiguration();
+
+        $linter = new LineLinter($configuration, false);
+
+        $line = 'a';
+        $input = new LineLinterInput(1, $line, 2, 0);
+        $result = $linter->fixLineIndentation($input);
+
+        $this->assertInstanceOf(LineLinterResult::class, $result);
+        $this->assertEquals(LineLinter::OPERATION_IGNORED, $result->operationPerformed);
+        $this->assertEquals('a', $result->lintedLine);
+    }
+
+    /**
+     * @return LineLinter
+     */
+    private function buildConfiguration()
+    {
+        $configuration = [
+            LineLinterConfigurationItem::TYPE_IGNORE_CHAR => ['a',],
+            LineLinterConfigurationItem::TYPE_OPENING_CHAR => ['b'],
+            LineLinterConfigurationItem::TYPE_OPEN_AND_CLOSE_CHAR => ['c'],
+            LineLinterConfigurationItem::TYPE_CLOSING_CHAR => ['d'],
+        ];
+
+        $configurationItems = [];
+
+        foreach ($configuration as $configurationType => $items) {
+            foreach ($items as $item) {
+                $configurationItems[] = new LineLinterConfigurationItem(
+                    $item,
+                    $configurationType
+                );
+            }
+        }
+
+        $configuration = new LineLinterConfiguration($configurationItems, 2);
+
+        return $configuration;
+    }
+}
